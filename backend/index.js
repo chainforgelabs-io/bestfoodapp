d708789757dc9e2ae710dcd045fad419c1714e18
Carson Halter <84420466+cah311@users.noreply.github.com>
2024-08-27 23:10:28 -0600

    Set mondoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Use the environment variable for the MongoDB connection string
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB", err));
+
 app.get("/", (req, res) => {
-  res.send("Backend is running!");
+  res.send("Backend is running and connected to MongoDB!");
 });
 
 app.listen(PORT, () => {
